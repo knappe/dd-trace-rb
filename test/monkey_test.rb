3dc2975ec1c9e410b80fe88cfd96848e2b03b483
Emanuele Palazzetti <emanuele.palazzetti@datadoghq.com>
2017-08-11 20:15:40 +0200

    [mongodb] update monkey tests

--- test/monkey_test.rb
+++ test/monkey_test.rb
@@ -8,7 +8,7 @@ require 'redis'
 class MonkeyTest < Minitest::Test
   def test_autopatch_modules
     assert_equal(
-      { elasticsearch: true, http: true, redis: true, grape: true, active_record: false },
+      { elasticsearch: true, http: true, redis: true, grape: true, mongo: true, active_record: false },
       Datadog::Monkey.autopatch_modules
     )
   end
@@ -24,7 +24,7 @@ class MonkeyTest < Minitest::Test
     assert_equal(false, Datadog::Contrib::Redis::Patcher.patched?)
     assert_equal(false, Datadog::Contrib::Grape::Patcher.patched?)
     assert_equal(false, Datadog::Contrib::ActiveRecord::Patcher.patched?)
-    assert_equal({ elasticsearch: false, http: false, redis: false, grape: false, active_record: false }, Datadog::Monkey.get_patched_modules())
+    assert_equal({ elasticsearch: false, http: false, redis: false, grape: false, mongo: false, active_record: false }, Datadog::Monkey.get_patched_modules())
 
     Datadog::Monkey.patch_module(:redis)
     assert_equal(false, Datadog::Contrib::Elasticsearch::Patcher.patched?)
@@ -32,7 +32,7 @@ class MonkeyTest < Minitest::Test
     assert_equal(true, Datadog::Contrib::Redis::Patcher.patched?)
     assert_equal(false, Datadog::Contrib::Grape::Patcher.patched?)
     assert_equal(false, Datadog::Contrib::ActiveRecord::Patcher.patched?)
-    assert_equal({ elasticsearch: false, http: false, redis: true, grape: false, active_record: false }, Datadog::Monkey.get_patched_modules())
+    assert_equal({ elasticsearch: false, http: false, redis: true, grape: false, mongo: false, active_record: false }, Datadog::Monkey.get_patched_modules())
 
     # now do it again to check it's idempotent
     Datadog::Monkey.patch_module(:redis)
@@ -41,7 +41,7 @@ class MonkeyTest < Minitest::Test
     assert_equal(true, Datadog::Contrib::Redis::Patcher.patched?)
     assert_equal(false, Datadog::Contrib::Grape::Patcher.patched?)
     assert_equal(false, Datadog::Contrib::ActiveRecord::Patcher.patched?)
-    assert_equal({ elasticsearch: false, http: false, redis: true, grape: false, active_record: false }, Datadog::Monkey.get_patched_modules())
+    assert_equal({ elasticsearch: false, http: false, redis: true, grape: false, mongo: false, active_record: false }, Datadog::Monkey.get_patched_modules())
 
     Datadog::Monkey.patch(elasticsearch: true, redis: true)
     assert_equal(true, Datadog::Contrib::Elasticsearch::Patcher.patched?)
@@ -49,7 +49,7 @@ class MonkeyTest < Minitest::Test
     assert_equal(true, Datadog::Contrib::Redis::Patcher.patched?)
     assert_equal(false, Datadog::Contrib::Grape::Patcher.patched?)
     assert_equal(false, Datadog::Contrib::ActiveRecord::Patcher.patched?)
-    assert_equal({ elasticsearch: true, http: false, redis: true, grape: false, active_record: false }, Datadog::Monkey.get_patched_modules())
+    assert_equal({ elasticsearch: true, http: false, redis: true, grape: false, mongo: false, active_record: false }, Datadog::Monkey.get_patched_modules())
 
     # verify that active_record is not auto patched by default
     Datadog::Monkey.patch_all()
@@ -58,7 +58,7 @@ class MonkeyTest < Minitest::Test
     assert_equal(true, Datadog::Contrib::Redis::Patcher.patched?)
     assert_equal(false, Datadog::Contrib::Grape::Patcher.patched?)
     assert_equal(false, Datadog::Contrib::ActiveRecord::Patcher.patched?)
-    assert_equal({ elasticsearch: true, http: true, redis: true, grape: false, active_record: false }, Datadog::Monkey.get_patched_modules())
+    assert_equal({ elasticsearch: true, http: true, redis: true, grape: false, mongo: false, active_record: false }, Datadog::Monkey.get_patched_modules())
 
     Datadog::Monkey.patch_module(:active_record)
     assert_equal(true, Datadog::Contrib::Elasticsearch::Patcher.patched?)
@@ -66,6 +66,6 @@ class MonkeyTest < Minitest::Test
     assert_equal(true, Datadog::Contrib::Redis::Patcher.patched?)
     assert_equal(false, Datadog::Contrib::Grape::Patcher.patched?)
     assert_equal(true, Datadog::Contrib::ActiveRecord::Patcher.patched?)
-    assert_equal({ elasticsearch: true, http: true, redis: true, grape: false, active_record: true }, Datadog::Monkey.get_patched_modules())
+    assert_equal({ elasticsearch: true, http: true, redis: true, grape: false, mongo: false, active_record: true }, Datadog::Monkey.get_patched_modules())
   end
 end
